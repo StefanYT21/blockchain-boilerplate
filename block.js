03fb0761200fe2779db8102d7e224808f69adc85
Stefan Postolache <darius@postolache.net>
2021-06-30 08:53:15 +0300

    10. Genesis Block

--- block.js
+++ block.js
@@ -7,9 +7,19 @@ class Block {
     }
     toString() {
         return `Block -
-            Timestamp : ${this.timestamp}
-            Last Hash : ${this.lastHash.substring(0, 10)}
-            Hash      : ${this.hash.substring(0, 10)}
+            Timestamp : ${this.timestamp},
+            Last Hash : ${this.lastHash.substring(0, 10)},
+            Hash      : ${this.hash.substring(0, 10)},
             Data      : ${this.data}`;
       }
+    static genesis() {
+        return new this('timpZero','nimic','MihaiAlexandruHash',[]);
+    }
 }
+
+module.exports = Block;
+
+
+
+
+
