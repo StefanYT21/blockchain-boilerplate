d0fde49c4271c1998899b3d5fafa692b33d01ea9
Stefan Postolache <darius@postolache.net>
2021-07-01 12:04:45 +0300

    Lectia 2

--- app/index.js
+++ app/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
-const HTTP_PORT = process.env.HTTP_PORT || 3001;
+const HTTP_PORT = process.env.HTTP_PORT || 3003;
 
 const app = express();
 const bc = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${blok.toString()}`);
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
