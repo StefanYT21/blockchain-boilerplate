1b8333665cc000611051f9fba270b57fc9743f71
Stefan Postolache <darius@postolache.net>
2021-07-16 12:04:04 +0300

    Lectia dragonu

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -34,12 +34,29 @@ describe('Transaction', () => {
 
     describe('transacting with an amount that exceeds the balance', () => {
         beforeEach(() => {
-          amount = 50000;
-          transaction = Transaction.newTransaction(wallet, recipient, amount);
+            amount = 50000;
+            transaction = Transaction.newTransaction(wallet, recipient, amount);
         });
     
         it('does not create the transaction', () => {
-          expect(transaction).toEqual(undefined);
+            expect(transaction).toEqual(undefined);
+        });
+    });
+
+    describe('and updating a transaction', () => {
+        let nextAmount, nextRecipient;
+        beforeEach(() => {
+            nextAmount = 20;
+            nextRecipient = 'n3xt-4ddr355';
+            transaction = transaction.update(wallet, nextRecipient, nextAmount);
+        });
+      
+        it('subtracts the next amount from the sender’s output', () => {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - amount - nextAmount);
+        });
+      
+        it('outputs an amount for the next recipient', () => {
+            expect(transaction.outputs.find(output => output.address === nextRecipient).amount).toEqual(nextAmount);
         });
     });
 });
