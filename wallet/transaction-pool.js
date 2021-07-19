fed7b51d4655c2db861ee0cd3473025340b0c374
Stefan Postolache <darius@postolache.net>
2021-07-19 10:14:33 +0300

    Lectia 49

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -18,6 +18,11 @@ class TransactionPool
             this.transactions.push(transaction);
         }
     }
+
+    existingTransaction(address)
+    {
+        return this.transactions.find(transaction => transaction.input.address === address);
+    }
 }
 
 module.exports = TransactionPool;
